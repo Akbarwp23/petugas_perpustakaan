a1b9f1f500e71b73ca6c62f4401ca7c4325b3beb
Akbarwp23 <Akbarwahyup23@gmail.com>
2024-01-31 15:12:55 +0700

    commit 2

---
+++ lib/app/modules/peminjaman/views/peminjaman_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:petugas_perpustakaan_kelas_c/app/data/model/response_pinjam.dart';
+
+import '../../../routes/app_pages.dart';
+import '../controllers/peminjaman_controller.dart';
+
+class PeminjamanView extends GetView<PeminjamanController> {
+  const PeminjamanView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('PeminjamanView'),
+          centerTitle: true,
+        ),
+        body: controller.obx((state) => ListView.separated(
+          itemCount: state!.length,
+          itemBuilder: (context, index){
+            DataPinjam dataPinjam = state[index];
+            return ListTile(
+              title: Text("${dataPinjam.book?.judul}"),
+              subtitle: Text("Tanggal Pinjam : ${dataPinjam.tanggalPinjam}\n Tanggal Pinjam : ${dataPinjam.tanggalKembali}\n Status : ${dataPinjam.status}"),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider();
+          }
+        ))
+    );
+  }
+}
